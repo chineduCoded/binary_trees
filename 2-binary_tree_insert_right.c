1e8ed86a609069ee76552907141d01287731e1ba
chineduCoded <chinedujohn17@gmail.com>
2023-02-28 11:02:16 -0800

    An error fixed

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
+	new = binary_tree_node(value);
 	if (new == NULL)
 		return (NULL);
 
@@ -24,6 +24,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right->parent = new;
 	}
 	parent->right = new;
+	new->parent = parent;
 
 	return (new);
 }
